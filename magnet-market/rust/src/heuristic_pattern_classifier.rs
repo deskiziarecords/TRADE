e6b197f1832afa87aaf3bab842d5ecf74f403579
J Roberto Jimenez <tijuanapaint@gmail.com>
2026-04-01 13:53:57 -0600

    Create heuristic_pattern_classifier.rs

---
+++ magnet-market/rust/src/heuristic_pattern_classifier.rs
@@ -0,0 +1,7 @@
+// heuristic_pattern_classifier.rs – Alias for s_and_d_psi.rs
+
+// use s_and_d_psi::detect_liquidity_blobs; // Uncomment to use the function
+
+pub mod prelude {
+    pub use crate::s_and_d_psi::detect_liquidity_blobs; // Re-exporting the function
+}
